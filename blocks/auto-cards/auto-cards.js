387697733b4760cbc0edbdb124e81a127aecb9bf
sgotenks <pellerei.luca@gmail.com>
2024-08-08 10:36:17 +0200

    Create auto-cards.js

---
+++ blocks/auto-cards/auto-cards.js
@@ -0,0 +1,74 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+
+export default async function decorate(block) {
+  const isIconCards = block.classList.contains('icon');
+  const isArticleCards = block.classList.contains('articles');
+
+  async function fetchJson(link) {
+    const response = await fetch(link?.href);
+
+    if (response.ok) {
+      const jsonData = await response.json();
+      const data = jsonData?.data;
+      return data;
+    }
+    return 'an error occurred';
+  }
+
+  const ul = document.createElement('ul');
+
+  if (isIconCards) {
+    [...block.children].forEach((row) => {
+      const anchor = document.createElement('a');
+      anchor.href = '';
+      const li = document.createElement('li');
+      while (row.firstElementChild) li.append(row.firstElementChild);
+      [...li.children].forEach((div) => {
+        if (div.children.length === 1 && div.querySelector('a')) {
+          const linkURL = div.querySelector('a').innerHTML;
+          anchor.href = linkURL;
+          div.className = 'cards-hide-markdown';
+        } else if (div.children.length === 1 && div.querySelector('picture')) {
+          div.className = 'cards-card-image';
+        } else if (div.children.length === 1 && div.querySelector('span')) {
+          div.className = 'cards-card-icon';
+        } else {
+          div.className = 'cards-card-body';
+        }
+      });
+      anchor.append(li);
+      ul.append(anchor);
+    });
+  }
+
+  if (isArticleCards) {
+    const link = block.querySelector('a');
+    const cardData = await fetchJson(link);
+    cardData.forEach((item) => {
+      const picture = createOptimizedPicture(item.image, item.title, false, [{ width: 320 }]);
+      picture.lastElementChild.width = '320';
+      picture.lastElementChild.height = '180';
+      const createdCard = document.createElement('li');
+      createdCard.innerHTML = `
+        <div class="cards-card-image">
+          <div data-align="center">${picture.outerHTML}</div>
+        </div>
+        <div class="cards-card-body">
+          <h5>${item.title}</h5>
+          <p class="button-container">
+            <a href="${item.url}" aria-label="${item['anchor-text']}" title="${item['anchor-text']}" class="button">
+              Read more 
+              <span class="card-arrow">
+                <img class="icon" src="/icons/chevron.svg" />
+              </span>
+            </a>
+          </p>
+        </div>
+      `;
+      ul.append(createdCard);
+    });
+  }
+
+  block.textContent = '';
+  block.append(ul);
+}
