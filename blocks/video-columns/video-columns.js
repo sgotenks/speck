fd979fcf48c1a273165676e6b369425f93af94e0
sgotenks <pellerei.luca@gmail.com>
2024-08-09 12:21:22 +0200

    Update video-columns.js

--- blocks/video-columns/video-columns.js
+++ blocks/video-columns/video-columns.js
@@ -1,7 +1,7 @@
 export default async function decorate(block) {
 
   const rows = [...block.children];
-  if (rows == 2){
+  if (rows.length == 2){
       block.firstElementChild.classList.add("mainTitle");
   }
   
