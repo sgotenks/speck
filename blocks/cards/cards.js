98282fa16a40b52fc2fd81bb7259cf26833de41b
gscamurra <giovanni.scamurra.adobe@gmail.com>
2024-08-20 13:05:31 +0200

    Update cards.js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -4,7 +4,7 @@ export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
   const title = document.createElement('div');
-  var titleBlock = "Title";
+  var titleBlock = "";
   title.append(titleBlock);
   title.classList.add("cardsTitle");
   [...block.children].forEach((row) => {
