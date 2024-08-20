aee2eae8e32a06bcd478ccf8b5318930eaf099cd
gscamurra <giovanni.scamurra.adobe@gmail.com>
2024-08-20 13:35:45 +0200

    add primopiano block

---
+++ blocks/primopiano/primopiano.js
@@ -0,0 +1,25 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+
+export default function decorate(block) {
+  /* change to ul, li */
+  const ul = document.createElement('ul');
+  const title = document.createElement('div');
+  var titleBlock = "Section";
+  title.append(titleBlock);
+  title.classList.add("cardsTitle");
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    while (row.firstElementChild) li.append(row.firstElementChild);
+    [...li.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      else div.className = 'cards-card-body';
+    });
+    ul.append(li);
+  });
+  ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  block.textContent = '';  
+  if (title != "" && title != undefined) {
+    block.append(title); 
+   }
+  block.append(ul);
+}
